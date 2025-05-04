17ec9a58b0da348ab193048092ecbe955e33712d
Foysal Ahmmed Limon <afaysal410@gmail.com>
2025-05-04 23:22:29 +0600

    update first week

---
+++ 6.py
@@ -0,0 +1,11 @@
+# Immutable
+a = "Hello"
+b = a
+a = "Hi"
+print(b)
+
+# Mutable
+list1 = [1, 2, 3]
+list2 = list1
+list1.append(4)
+print(list2)
---
+++ 8.py
@@ -0,0 +1,14 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+operation = input("Choose operation (+, -, *, /): ")
+
+if operation == "+":
+    print("Result:", num1 + num2)
+elif operation == "-":
+    print("Result:", num1 - num2)
+elif operation == "*":
+    print("Result:", num1 * num2)
+elif operation == "/":
+    print("Result:", num1 / num2)
+else:
+    print("Invalid operation")
